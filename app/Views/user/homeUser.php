5210a2610fbad29965f8211381cd2ebc5e413d22
rubenboediono <rubentricahya@gmail.com>
2024-05-19 21:00:07 +0700

    COMMIT FORM

--- app/Views/user/homeUser.php
+++ app/Views/user/homeUser.php
@@ -70,7 +70,7 @@
                         <!-- End Col -->
 
                         <div class="sm:col-span-9">
-                            <input id="af-account-email" type="email" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-teal-600 focus:ring-teal-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="maria@site.com" value="<?= $akun_data['email'] ?? '' ?>" readonly>
+                            <input id="af-account-email" type="email" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:outline-teal-600 focus:ring-teal-600 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="maria@site.com" value="<?= $akun_data['email'] ?? '' ?>" readonly>
                         </div>
                         <!-- End Col -->
 
@@ -142,7 +142,7 @@
                         <!-- End Col -->
 
                         <div class="sm:col-span-9">
-                            <input id="af-account-email" type="email" name="email" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="maria@site.com" value="<?= $akun_data['email'] ?? '' ?>">
+                            <input id="af-account-email" type="email" name="email" class="py-2 px-3 pe-11 block w-full border-gray-200 shadow-sm text-sm rounded-lg focus:outline-blue-500 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="maria@site.com" value="<?= $akun_data['email'] ?? '' ?>">
                         </div>
                         <!-- End Col -->
 
