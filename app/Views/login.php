c18325b69cbad03829a7b4d55eaa3aca03925bcd
rubenboediono <rubentricahya@gmail.com>
2024-06-22 23:55:36 +0700

    COMMIT LOGIN FORM

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,21 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url() ?>css/style.css?v=1.0">
     <title>Halaman Login</title>
+
+    <style>
+    .bg-svg {
+        background-image: url('/svg/background.svg'); /* Adjust the path based on your directory structure */
+        background-size: cover; /* Adjust as necessary */
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+</style>
 </head>
 
-<body class="bg-[#E2E8F0] font-gilroy">
+<body class="bg-svg font-gilroy">
 
-    <div class="bg-white min-h-screen flex items-center justify-center">
+    <div class="min-h-screen flex items-center justify-center">
         <form action="dashboard" method="post" class="w-full max-w-md">
             <div class="px-8 py-10 bg-white shadow-lg rounded-xl">
-                <h2 class="text-3xl font-semibold text-center mb-6">Login</h2>
+                <h2 class="text-3xl font-bold text-center mb-6">Masuk ke akun Anda</h2>
                 <div class="mb-6">
                     <label for="nip" class="block text-gray-600 mb-1">Email</label>
-                    <input id="email" name="email" type="text" placeholder="Enter your Email" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-black "required>
+                    <input id="email" name="email" type="text" placeholder="Alamat Email" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-black "required>
                 </div>
                 <div class="mb-6">
                     <label for="password" class="block text-gray-600 mb-1">Password</label>
-                    <input id="password" name="password" type="password" placeholder="Enter your password" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-black "required>
+                    <input id="password" name="password" type="password" placeholder="Kata Sandi" class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-black "required>
                 </div>
                 <div class="text-center">
                     <button type="submit" class="w-full px-6 py-3 bg-teal-800  text-white font-semibold rounded-md hover:bg-indigo-600 transition duration-200 ease">Submit</button>
