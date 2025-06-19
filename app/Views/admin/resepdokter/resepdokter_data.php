3aaf3270773f2ac2d72cdd17ea9f765e91cbfd4a
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-06-19 16:48:14 +0700

    Remove redundant scripts that is already in View/components/notif

--- app/Views/admin/resepdokter/resepdokter_data.php
+++ app/Views/admin/resepdokter/resepdokter_data.php
@@ -171,31 +171,4 @@
 <!-- End Card -->
 
 <!-- End Table Section -->
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        // Menghitung jumlah div elemen di dalam kadaluwarsa-content
-        var kadaluwarsaContent = document.getElementById('kadaluwarsa-content');
-        var divCount = kadaluwarsaContent.querySelectorAll('div.kadaluwarsabaris, a.kadaluwarsabaris').length;
-
-        // Memperbarui teks dalam SVG
-        var svgText = document.querySelector('#kadaluwarsa-tab svg text');
-        svgText.textContent = divCount.toString();
-    });
-    // JavaScript to toggle between tabs
-    document.getElementById('stok-tab').addEventListener('click', function() {
-        document.getElementById('stok-content').classList.remove('hidden');
-        document.getElementById('kadaluwarsa-content').classList.add('hidden');
-        this.classList.add('border-[#272727]');
-        document.getElementById('kadaluwarsa-tab').classList.remove('border-[#272727]');
-    });
-
-    document.getElementById('kadaluwarsa-tab').addEventListener('click', function() {
-        document.getElementById('stok-content').classList.add('hidden');
-        document.getElementById('kadaluwarsa-content').classList.remove('hidden');
-        this.classList.add('border-[#272727]');
-        document.getElementById('stok-tab').classList.remove('border-[#272727]');
-    });
-
-    
-</script>
 <?= $this->endSection(); ?>
--- app/Views/admin/rujukan/rujukan_keluar_data.php
+++ app/Views/admin/rujukan/rujukan_keluar_data.php
@@ -292,31 +292,5 @@
         var count_notif_stok = <?= $count_notif_stok ?>;
         document.querySelector('#stok-tab svg text').textContent = count_notif_stok;
     });
-
-    document.addEventListener('DOMContentLoaded', function() {
-        // Menghitung jumlah div elemen di dalam kadaluwarsa-content
-        var kadaluwarsaContent = document.getElementById('kadaluwarsa-content');
-        var divCount = kadaluwarsaContent.querySelectorAll('div.kadaluwarsabaris, a.kadaluwarsabaris').length;
-
-        // Memperbarui teks dalam SVG
-        var svgText = document.querySelector('#kadaluwarsa-tab svg text');
-        svgText.textContent = divCount.toString();
-    });
-    // JavaScript to toggle between tabs
-    document.getElementById('stok-tab').addEventListener('click', function() {
-        document.getElementById('stok-content').classList.remove('hidden');
-        document.getElementById('kadaluwarsa-content').classList.add('hidden');
-        this.classList.add('border-[#272727]');
-        document.getElementById('kadaluwarsa-tab').classList.remove('border-[#272727]');
-    });
-
-    document.getElementById('kadaluwarsa-tab').addEventListener('click', function() {
-        document.getElementById('stok-content').classList.add('hidden');
-        document.getElementById('kadaluwarsa-content').classList.remove('hidden');
-        this.classList.add('border-[#272727]');
-        document.getElementById('stok-tab').classList.remove('border-[#272727]');
-    });
-
-    
 </script>
 <?= $this->endSection(); ?>
