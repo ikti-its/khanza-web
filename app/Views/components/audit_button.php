cb77018dbf79e9bbfc9c6b0253c6209d563de923
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-06-26 15:28:15 +0700

    Change audit symbol from plus symbol to eye symbol

--- app/Views/components/audit_button.php
+++ app/Views/components/audit_button.php
@@ -1,7 +1,8 @@
 <div>
-    <a href= <?= $link ?> class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#0A2D27] text-[#ACF2E7] hover:bg-[#13594E] disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-        <svg class="flex-shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="16" height="1" viewBox="0 0 16 16" fill="none">
-            <path d="M2.63452 7.50001L13.6345 7.5M8.13452 13V2" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+    <a href="<?= $link ?>" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#0A2D27] text-[#ACF2E7] hover:bg-[#13594E] disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            d="M1.5 12C3.6 7.5 7.5 4.5 12 4.5s8.4 3 10.5 7.5c-2.1 4.5-6 7.5-10.5 7.5S3.6 16.5 1.5 12zM12 15a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
         </svg>
         Audit
     </a>
