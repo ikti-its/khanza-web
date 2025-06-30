6022312886fb1bf4356fe948b4e38e03babd968c
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-06-30 14:37:20 +0700

    Refactor menu and svg images

--- app/Views/components/menu/submenu_baris.php
+++ app/Views/components/menu/submenu_baris.php
@@ -1,6 +1,8 @@
 <li>
     <a href="<?= esc($link) ?>" class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:text-slate-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-        <img src="<?= base_url('svg/' . $icon . '.svg') ?>">    
+        <?php if($icon !== ''){
+            echo '<img src="' . base_url('svg/' . $icon . '.svg') .'">';
+        } ?>
         <?= esc($teks) ?>
     </a>
 </li>
