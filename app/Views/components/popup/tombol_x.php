a6bb63a4c4c72247726cde87547160a442a6f160
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-07-01 18:50:20 +0700

    Move inline svgs in Views/components to public/svg

--- app/Views/components/popup/tombol_x.php
+++ app/Views/components/popup/tombol_x.php
@@ -1,8 +1,5 @@
 <button type="button" class="flex justify-center items-center size-7 text-sm font-semibold rounded-full border border-transparent text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700" 
     data-hs-overlay="#hs-vertically-centered-scrollable-modal-<?= $id ?>">
     <span class="sr-only">Close</span>
-    <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M18 6 6 18"></path>
-        <path d="m6 6 12 12"></path>
-    </svg>
+    <img src="<?= base_url('svg/popup_tombol_x.svg') ?>">
 </button>
