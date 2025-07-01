f2ef207e9b7716b62ef297319545720702027b5c
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-07-01 14:01:12 +0700

    Fix ubah & tambah forms when the fields are odd

--- app/Views/components/form/isian/kosong.php
+++ app/Views/components/form/isian/kosong.php
@@ -3,5 +3,4 @@
     type="hidden" 
     class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2 w-full lg:w-1/4 dark:border-gray-600 dark:text-white" 
     maxlength="80" 
-    required
 >
