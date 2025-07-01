19b6a8708a27b614d6b45749a02a38b4c78d1af0
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-07-01 18:25:47 +0700

    Move inline svgs in Views/components/header to public/svg

--- app/Views/components/header/audit_button.php
+++ app/Views/components/header/audit_button.php
@@ -1,9 +1,6 @@
 <div>
     <a href="<?= $link ?>" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-[#0A2D27] text-[#ACF2E7] hover:bg-[#13594E] disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-            d="M1.5 12C3.6 7.5 7.5 4.5 12 4.5s8.4 3 10.5 7.5c-2.1 4.5-6 7.5-10.5 7.5S3.6 16.5 1.5 12zM12 15a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-        </svg>
+        <img src="<?= base_url('svg/audit_button_icon.svg')?>"> 
         Audit
     </a>
 </div>
