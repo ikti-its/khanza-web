8aecda8dbd9e7581189fb5e33bb01008d4184c83
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2025-07-02 22:17:42 +0700

    Add Kembali button in audit pages

--- app/Views/components/header/audit.php
+++ app/Views/components/header/audit.php
@@ -2,7 +2,12 @@
 <div class="py-1 flex justify-between items-center border-gray-200 dark:border-gray-700">
     <?= view('components/header/judul', [
         'judul' => $judul
-     ]) ?>
+    ]) ?>
+    <div class="flex gap-x-6 justify-center items-center">
+        <?= view('components/header/kembali', [
+            'link' => $modul_path
+        ]) ?>
+    </div>
 </div>
 
 <!-- End Header -->
