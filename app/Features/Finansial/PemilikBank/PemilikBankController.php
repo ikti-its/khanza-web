dfcb33a14bb44f80952786c35f3aecd1c7dac552
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-06-06 01:21:19 +0700

    Complete feature Finansial/PemilikBank

---
+++ app/Features/Finansial/PemilikBank/PemilikBankController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Finansial\PemilikBank;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class PemilikBankController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new PemilikBankModel(),
+            [
+                ['Finansial', 'finansial'],
+                ['Pemilik Bank', 'pemilik_bank'],
+            ],
+            'Pemilik Bank',
+            [
+                A::READ,
+                A::CREATE,
+                A::AUDIT,
+                A::UPDATE,
+                A::DELETE,
+            ],
+            [
+                [SHOW, REQUIRED, I::INDEX, 'id_pemilik',   'ID'],
+                [SHOW, REQUIRED, I::TEXT,  'nama_pemilik', 'Pemilik'],
+            ],
+        );
+    }
+}
