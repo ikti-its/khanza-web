ee3f1f37d9e3f3a8d658011e448cefecebf0ef3a
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-06-06 01:21:27 +0700

    Complete feature Finansial/PrinsipBank

---
+++ app/Features/Finansial/PrinsipBank/PrinsipBankController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Finansial\PrinsipBank;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class PrinsipBankController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new PrinsipBankModel(),
+            [
+                ['Finansial', 'finansial'],
+                ['Prinsip Bank', 'prinsip_bank'],
+            ],
+            'Prinsip Bank',
+            [
+                A::READ,
+                A::CREATE,
+                A::AUDIT,
+                A::UPDATE,
+                A::DELETE,
+            ],
+            [
+                [SHOW, REQUIRED, I::INDEX, 'id_prinsip',   'ID'],
+                [SHOW, REQUIRED, I::TEXT,  'nama_prinsip', 'Prinsip'],
+            ],
+        );
+    }
+}
