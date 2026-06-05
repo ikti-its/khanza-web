430a3f9ec1e2f174f7dc7a30df5e1979e5ee57ff
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-06-06 02:25:39 +0700

    Complete feature Kontak/Telepon

---
+++ app/Features/Kontak/Telepon/TeleponController.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Kontak\Telepon;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class TeleponController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new TeleponModel(),
+            [
+                ['Kontak', 'kontak'],
+                ['Telepon', 'telepon'],
+            ],
+            'Telepon',
+            [
+                A::READ,
+                A::CREATE,
+                A::AUDIT,
+                A::UPDATE,
+                A::DELETE,
+            ],
+            [
+                [SHOW, REQUIRED, I::INDEX,  'id_telepon',    'ID'],
+                [SHOW, REQUIRED, I::TEXT,   'nomor_telepon', 'Nomor'],
+                [SHOW, REQUIRED, I::TEXT,   'nama_jenis',    'Jenis Telepon'],
+                [SHOW, REQUIRED, I::TEXT,   'nama_provider', 'Provider'],
+                [SHOW, REQUIRED, I::TEXT,   'nik',           'NIK'],
+                [SHOW, REQUIRED, I::TEXT,   'nama',          'Nama Lengkap'],
+            ],
+        );
+    }
+}
