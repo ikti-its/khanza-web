7f6f3e661f12e3e096579112b21d871d62f36834
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-06-06 01:21:05 +0700

    Complete feature Finansial/Bank

---
+++ app/Features/Finansial/Bank/BankController.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Finansial\Bank;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class BankController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new BankModel(),
+            [
+                ['Finansial', 'finansial'],
+                ['Bank', 'bank'],
+            ],
+            'Bank',
+            [
+                A::READ,
+                A::CREATE,
+                A::AUDIT,
+                A::UPDATE,
+                A::DELETE,
+            ],
+            [
+                [SHOW, REQUIRED, I::TEXT, 'kode_bank',      'Kode'],
+                [SHOW, REQUIRED, I::TEXT, 'nama_bank',      'Bank'],
+                [SHOW, REQUIRED, I::TEXT, 'sebutan',        'Singkatan'],
+                [SHOW, REQUIRED, I::TEXT, 'nama_pemilik',   'Pemilik'],
+                [SHOW, REQUIRED, I::TEXT, 'nama_prinsip',   'Prinsip'],
+                [SHOW, REQUIRED, I::BOOL, 'is_bank_devisa', 'Bank Devisa'],
+                [SHOW, REQUIRED, I::TEXT, 'mobile_app',     'Aplikasi Mobile'],
+                [SHOW, REQUIRED, I::TEXT, 'link_playstore', 'Playstore'],  
+            ],
+        );
+    }
+}
