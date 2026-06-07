27c21882d93de15c9087d30573d209ed06afb45f
qoyyimil <qoyyimillj@gmail.com>
2026-06-08 00:40:45 +0700

    Add Registrasi feature

---
+++ app/Features/RekamMedis/Registrasi/RegistrasiController.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RekamMedis\Registrasi;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class RegistrasiController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new RegistrasiModel(),
+            [
+                ['Rekam Medis', 'rekam_medis'],
+                ['Registrasi',  'registrasi'],
+            ],
+            'Registrasi',
+            [
+                A::READ,
+                A::CREATE,
+                A::AUDIT,
+                A::UPDATE,
+                A::DELETE,
+            ],
+            [
+                [HIDE, OPTIONAL, I::INDEX,  'id_registrasi',     'ID Registrasi'],
+                [SHOW, REQUIRED, I::TEXT,   'nomor_reg',         'Nomor Registrasi'],
+                [SHOW, REQUIRED, I::TEXT,   'nomor_rawat',       'Nomor Rawat'],
+                [SHOW, REQUIRED, I::DTIME,  'datetime',          'Tanggal & Jam'],
+                [SHOW, REQUIRED, I::SELECT, 'id_dokter',         'Dokter'],
+                [SHOW, REQUIRED, I::SELECT, 'id_pasien',         'Pasien'],
+                [SHOW, REQUIRED, I::SELECT, 'id_poliklinik',     'Poliklinik'],
+                [SHOW, REQUIRED, I::SELECT, 'id_pj_pasien',      'Penanggung Jawab'],
+                [SHOW, REQUIRED, I::SELECT, 'id_alamat_pj',      'Alamat PJ'],
+                [SHOW, REQUIRED, I::TEXT,   'hubungan_pj',       'Hubungan PJ'],
+                [SHOW, OPTIONAL, I::TEXT,   'pekerjaanpj',       'Pekerjaan PJ'],
+                [SHOW, OPTIONAL, I::TEXT,   'notelp_pj',         'No. Telp PJ'],
+                [SHOW, OPTIONAL, I::TEXT,   'no_asuransi',       'No. Asuransi'],
+                [SHOW, OPTIONAL, I::TEXT,   'no_telepon',        'No. Telepon Pasien'],
+                [SHOW, OPTIONAL, I::TEXT,   'biaya_registrasi',  'Biaya Registrasi'],
+                [SHOW, REQUIRED, I::TEXT,   'jenis_bayar',       'Jenis Bayar'],
+                [SHOW, REQUIRED, I::TEXT,   'status_registrasi', 'Status Registrasi'],
+                [SHOW, REQUIRED, I::TEXT,   'status_rawat',      'Status Rawat'],
+                [SHOW, REQUIRED, I::TEXT,   'status_poli',       'Status Poliklinik'],
+                [SHOW, REQUIRED, I::TEXT,   'status_bayar',      'Status Bayar'],
+            ],
+        );
+    }
+}
---
+++ app/Features/RekamMedis/Registrasi/RegistrasiModel.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RekamMedis\Registrasi;
+
+use App\Core\Model\ModelTemplate;
+use App\Core\Model\ValidationType as V;
+
+final class RegistrasiModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new RegistrasiDatabase(),
+            [
+                'id_registrasi'     => V::DEFAULT(),
+                'nomor_reg'         => V::DEFAULT(),
+                'nomor_rawat'       => V::DEFAULT(),
+                'datetime'          => V::DEFAULT(),
+                'hubungan_pj'       => V::DEFAULT(),
+                'pekerjaanpj'       => V::DEFAULT(),
+                'notelp_pj'         => V::DEFAULT(),
+                'no_asuransi'       => V::DEFAULT(),
+                'no_telepon'        => V::DEFAULT(),
+                'biaya_registrasi'  => V::DEFAULT(),
+                'jenis_bayar'       => V::DEFAULT(),
+                'status_registrasi' => V::DEFAULT(),
+                'status_rawat'      => V::DEFAULT(),
+                'status_poli'       => V::DEFAULT(),
+                'status_bayar'      => V::DEFAULT(),
+            ],
+            [
+                'id_dokter'     => [
+                    'kode_dokter',
+                    'id_orang'  => ['nama'],
+                    'spesialis'
+                ],
+                'id_pasien'     => [
+                    'id_orang'  => ['nama'],
+                    'nomor_rm'
+                ],
+                'id_pj_pasien'  => ['nama'],
+                'id_alamat_pj'  => ['alamat_lengkap'],
+                'id_poliklinik' => ['kode_poliklinik', 'nama_poliklinik'],
+            ],
+        );
+    }
+}
