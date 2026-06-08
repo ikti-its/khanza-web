5a0257b1a4d4f194a5444489b5e291c3b0826d64
mas663 <affan8971@gmail.com>
2026-06-08 13:12:28 +0700

    fix: remove duplicate value attr and normalize datetime to date in tanggal input

--- app/Views/components/form/isian/tanggal.php
+++ app/Views/components/form/isian/tanggal.php
@@ -11,9 +11,8 @@
     id="<?= $id ?>"
     type="date"
     name="<?= $column ?>"
-    value="<?= $value ?>"
-    value="<?= date('Y-m-d') ?>"
+    value="<?= $value ? substr((string) $value, 0, 10) : '' ?>"
     class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2 w-full lg:w-1/4 dark:border-gray-600 dark:text-white"
     maxlength="80"
-    <?= $req  === 1   ? 'required' : ''; ?>
+    <?= $req === 1 ? 'required' : ''; ?>
 >
