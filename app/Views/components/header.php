da542521f5b9c0078de55b332e162f6a34ee27cc
qoyyimil <qoyyimillj@gmail.com>
2026-06-08 16:42:33 +0700

    Remove redundant classes in header layout

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -1,26 +1,20 @@
 <body class="bg-gray-50 dark:bg-slate-900">
-    <!-- ========== HEADER ========== -->
     <header class="sticky top-0 inset-x-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-[48] w-full bg-white border-b text-sm py-2.5 sm:py-4 lg:ps-64 dark:bg-gray-800 dark:border-gray-700">
-        <nav class="flex basis-full items-center w-full mx-auto px-4 sm:px-6 md:px-8" aria-label="Global">
+        <nav class="flex items-center w-full mx-auto px-4 sm:px-6 md:px-8" aria-label="Global">
             <div class="me-5 lg:me-0 lg:hidden">
                 <a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">
                     <img src="/img/logo-omnia.png" class="h-4">
                 </a>
             </div>
 
-            <div class="w-full flex items-center justify-end ms-auto sm:justify-end sm:gap-x-3 sm:order-3">
-                <div class="flex flex-row items-center justify-end gap-2">
+            <div class="w-full flex items-center justify-end ms-auto sm:gap-x-3 sm:order-3">
+                <div class="flex items-center justify-end gap-2">
                     <?= $this->include('components/notif') ?>
                     <?= $this->include('components/profile') ?>
                 </div>
             </div>
         </nav>
     </header>
-    <!-- ========== END HEADER ========== -->
-
-    <!-- ========== MAIN CONTENT ========== -->
 
     <?= $this->include('components/sidebar') ?>
-
-    <!-- ========== END MAIN CONTENT ========== -->
 </body>
