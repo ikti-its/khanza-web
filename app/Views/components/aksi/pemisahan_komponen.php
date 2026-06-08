a3a421f4adfb09e6e8dec0a66cc54f7abb664de6
Aliyah <aliyahawwaliyah@gmail.com>
2026-06-08 15:20:34 +0700

    Correct base URL for pemisahan komponen button link

--- app/Views/components/aksi/pemisahan_komponen.php
+++ app/Views/components/aksi/pemisahan_komponen.php
@@ -1,7 +1,7 @@
 <div class="w-full">
     <?php if (isset($baris['id_status_pengambilan']) && (int)$baris['id_status_pengambilan'] === 1): ?>
         <div class="px-3 py-1.5">
-            <a href="<?= base_url('inventori darah/pemisahan komponen/tambah?pengambilan=' . $id) ?>" 
+            <a href="<?= base_url('inventori-darah/pemisahan-komponen/tambah?pengambilan=' . $id) ?>" 
                class="gap-x-1 text-sm text-teal-600 hover:underline font-semibold dark:text-teal-400">
                 Pisahkan
             </a>
