b3391da11e26a95fab396194e45e172b78d5d26d
qoyyimil <qoyyimillj@gmail.com>
2026-06-16 15:58:12 +0700

    Add StatusBayar to RekamMedis schema

---
+++ app/Features/RekamMedis/StatusBayar/StatusBayarModel.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RekamMedis\StatusBayar;
+
+use App\Core\Model\ModelTemplate;
+use App\Core\Model\ValidationType as V;
+
+final class StatusBayarModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new StatusBayarDatabase(),
+            [
+                'id_status_bayar'   => V::DEFAULT(),
+                'nama_status_bayar' => V::DEFAULT(),
+            ],
+            [],
+        );
+    }
+}
