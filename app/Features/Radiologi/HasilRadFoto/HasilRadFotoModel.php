698fd51588ae7567cf651a030ba59349bbe287e8
qoyyimil <qoyyimillj@gmail.com>
2026-06-19 11:23:38 +0700

    Add HasilRadFoto

---
+++ app/Features/Radiologi/HasilRadFoto/HasilRadFotoModel.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Radiologi\HasilRadFoto;
+
+use App\Core\Model\ModelTemplate;
+use App\Core\Model\ValidationType as V;
+
+final class HasilRadFotoModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new HasilRadFotoDatabase(),
+            [
+                'id_rad_foto' => V::DEFAULT(),
+                'nama_file'   => V::DEFAULT(),
+                'tgl_upload'  => V::DEFAULT(),
+            ],
+            [
+                'id_hasil_rad' => [],
+            ],
+        );
+    }
+}
