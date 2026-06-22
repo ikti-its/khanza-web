400349beca5a262b3963edd2a47c80da9a68c56f
qoyyimil <qoyyimillj@gmail.com>
2026-06-22 12:42:56 +0700

    Add StatusRawat for UGD

---
+++ app/Features/UGD/StatusRawat/StatusRawatController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UGD\StatusRawat;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class StatusRawatController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new StatusRawatModel(),
+            [
+                ['UGD',          'ugd'],
+                ['Status Rawat', 'status_rawat'],
+            ],
+            'Status Rawat',
+            [
+                A::READ,
+                // A::CREATE,
+                // A::AUDIT,
+                // A::UPDATE,
+                // A::DELETE,
+            ],
+            [
+                [HIDE, OPTIONAL, I::INDEX, 'id_status_rawat',   'ID Status Rawat'],
+                [SHOW, REQUIRED, I::TEXT,  'nama_status_rawat', 'Status Rawat'],
+            ],
+        );
+    }
+}
---
+++ app/Features/UGD/StatusRawat/StatusRawatDatabase.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\UGD\StatusRawat;
+
+use App\Core\Database\Template\DatabaseTemplate;
+use App\Core\Database\Template\SemanticType as T;
+
+final class StatusRawatDatabase extends DatabaseTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'ugd',
+            'status_rawat',
+            [
+                'id_status_rawat'   => T::ID(5),
+                'nama_status_rawat' => T::NAME(20),
+            ],
+            'id_status_rawat',
+            [],
+            [],
+            true,
+            'status_rawat.csv',
+        );
+    }
+}
