989c790cad2a3e7f4837755786b74c34c2aff873
qoyyimil <qoyyimillj@gmail.com>
2026-06-22 14:27:11 +0700

    Add modalalamat

---
+++ app/Views/components/modal/modalalamat.php
@@ -0,0 +1,33 @@
+<?= view('components/modal/modal-table', [
+    'modalId'      => 'modalAlamat',
+    'modalTitle'   => 'Pilih Alamat',
+    'headers'      => ['Kota/Kabupaten', 'Alamat Lengkap'],
+    'tableId'      => 'alamatTable',
+    'searchInputs' => [
+        ['id' => 'searchAlamatKota',    'placeholder' => 'Cari kota/kabupaten...'],
+        ['id' => 'searchAlamatLengkap', 'placeholder' => 'Cari alamat...'],
+    ],
+    'actions' => [
+        ['type' => 'button', 'text' => 'Refresh',      'onclick' => 'open_modalAlamat()', 'icon' => 'refresh'],
+        ['type' => 'link',   'text' => 'Tambah Alamat', 'href'   => '/lokasi/alamat/tambah', 'icon' => 'plus'],
+    ]
+]) ?>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        initModalList({
+            modalId:     'modalAlamat',
+            tableId:     'alamatTable',
+            url:         '<?= site_url('lokasi/alamat/modal/list') ?>',
+            fields:      ['nama_kota', 'alamat_lengkap'],
+            searchIds: {
+                searchAlamatKota:    'nama_kota',
+                searchAlamatLengkap: 'alamat_lengkap',
+            },
+            rowsPerPage: 10,
+            onSelect: (item) => {
+                autofillAlamatFields(item);
+            }
+        });
+    });
+</script>
