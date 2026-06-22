8f4545ec18cc31fa63f279bd57642043f3da6e57
qoyyimil <qoyyimillj@gmail.com>
2026-06-22 12:41:29 +0700

    Add HubunganPj for RM

---
+++ app/Features/RekamMedis/HubunganPj/HubunganPjController.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\RekamMedis\HubunganPj;
+
+use App\Core\Controller\ActionType as A;
+use App\Core\Controller\ControllerTemplate;
+use App\Core\Controller\InputType as I;
+
+final class HubunganPjController extends ControllerTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new HubunganPjModel(),
+            [
+                ['Rekam Medis',          'rekam_medis'],
+                ['Hubungan Penanggung Jawab', 'hubungan_pj'],
+            ],
+            'Hubungan Penanggung Jawab',
+            [
+                A::READ,
+                // A::CREATE,
+                // A::AUDIT,
+                // A::UPDATE,
+                // A::DELETE,
+            ],
+            [
+                [HIDE, OPTIONAL, I::INDEX, 'id_hubungan_pj',   'ID Hubungan PJ'],
+                [SHOW, REQUIRED, I::TEXT,  'nama_hubungan_pj', 'Hubungan Penanggung Jawab'],
+            ],
+        );
+    }
+}
