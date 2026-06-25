a1ac82e641b528381d95e49c7bb78408598bbe72
qoyyimil <qoyyimillj@gmail.com>
2026-06-25 21:24:39 +0700

    Rename TagihanOperasiPaket to TagihanOperasiTindakan

--- app/Features/Operasi/TagihanOperasiTindakan/TagihanOperasiTindakanDatabase.php
+++ app/Features/Operasi/TagihanOperasiTindakan/TagihanOperasiTindakanDatabase.php
@@ -1,24 +1,24 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Features\Operasi\TagihanOperasiPaket;
+namespace App\Features\Operasi\TagihanOperasiTindakan;
 
 use App\Core\Database\Template\DatabaseTemplate;
 use App\Core\Database\Template\SemanticType as T;
 
-final class TagihanOperasiPaketDatabase extends DatabaseTemplate
+final class TagihanOperasiTindakanDatabase extends DatabaseTemplate
 {
     public function __construct()
     {
         parent::__construct(
             'operasi',
-            'tagihan_operasi_paket',
+            'tagihan_operasi_tindakan',
             [
-                'id_tagihan_paket' => T::ID(300_000_000),
-                'id_tagihan'       => T::FK_AUTO(),
-                'id_paket'         => T::FK_AUTO(),
+                'id_tagihan_tindakan' => T::ID(300_000_000),
+                'id_tagihan'          => T::FK_AUTO(),
+                'id_tindakan'         => T::FK_AUTO(),
             ],
-            'id_tagihan_paket',
+            'id_tagihan_tindakan',
             [],
             [
                 [
@@ -27,9 +27,9 @@ final class TagihanOperasiPaketDatabase extends DatabaseTemplate
                     ['id_tagihan'],
                 ],
                 [
-                    ['id_paket'],
-                    \App\Features\Operasi\PaketTindakanOperasi\PaketTindakanOperasiDatabase::class,
-                    ['id_paket'],
+                    ['id_tindakan'],
+                    \App\Features\Operasi\RefTindakanOperasi\RefTindakanOperasiDatabase::class,
+                    ['id_tindakan'],
                 ],
             ],
         );
---
+++ app/Features/Operasi/TagihanOperasiTindakan/TagihanOperasiTindakanModel.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Features\Operasi\TagihanOperasiTindakan;
+
+use App\Core\Model\ModelTemplate;
+use App\Core\Model\ValidationType as V;
+
+final class TagihanOperasiTindakanModel extends ModelTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new TagihanOperasiTindakanDatabase(),
+            [
+                'id_tagihan_tindakan' => V::DEFAULT(),
+            ],
+            [
+                'id_tagihan'  => [],
+                'id_tindakan' => [],
+            ],
+        );
+    }
+}
