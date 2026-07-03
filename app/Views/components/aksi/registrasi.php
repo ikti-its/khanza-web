4ffa14a3d73fbdf64a77604e151d53ed36cb5585
qoyyimil <qoyyimillj@gmail.com>
2026-07-03 14:30:24 +0700

    Fix registrasi url

--- app/Views/components/aksi/registrasi.php
+++ app/Views/components/aksi/registrasi.php
@@ -4,7 +4,7 @@ $id_unit = (int) ($baris['id_unit'] ?? 0);
 
 $registrasi_url = (isset($baris['id_keputusan']) && (int) $baris['id_keputusan'] === 2)
     ? '/ugd/registrasi/tambah?no_rm=' . $no_rm
-    : '/rekam-medis/registrasi/tambah?no_rm=' . $no_rm . ($id_unit > 0 ? '&unit=' . $id_unit : '');
+    : '/registrasi/registrasi/tambah?no_rm=' . $no_rm . ($id_unit > 0 ? '&unit=' . $id_unit : '');
 ?>
 <div class="px-3 py-1.5">
     <a href="<?= $registrasi_url ?>" class="gap-x-1 text-sm text-green-600 decoration-2 hover:underline font-semibold dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
