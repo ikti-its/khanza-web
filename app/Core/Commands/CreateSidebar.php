df4f37534aff3abae9c123413dcad9f5344404ae
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-07-06 11:52:11 +0700

    Add omnia:sidebar command to generate sidebar

---
+++ app/Core/Commands/CreateSidebar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Core\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+
+class CreateSidebar extends BaseCommand
+{
+    protected $group = 'Omnia';
+    protected $name = 'omnia:sidebar';
+    protected $description = 'Append sidebar variable to App/Views/components/sidebar.php';
+
+    public function run(array $_params)
+    {
+        $route_group = new \App\Features\AllRoutes();
+        file_put_contents(
+            APPPATH . 'Config/GeneratedSidebar.php',
+            $route_group->generate_sidebar()
+        );
+    }
+}
