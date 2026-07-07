c58f15d44e031b4f63bb068a9a693f236f6b722e
ahmadhiandaffa <ahmadhian.daffa@gmail.com>
2026-07-07 18:12:27 +0700

    Refactor Encrypt migration to be compatible with commands

--- app/Core/Database/Special/EncryptDatabase.php
+++ app/Core/Database/Special/EncryptDatabase.php
@@ -2,39 +2,27 @@
 declare(strict_types=1);
 
 namespace App\Core\Database\Special;
-use App\Core\Database\Template\DatabaseTemplate;
 
-final class EncryptDatabase extends DatabaseTemplate
+final class EncryptDatabase
 {    
-    #[\Override()]
-    public function up(): void
+    public function run(string $arg): void
     {
-        // $config = new \Config\Database()->default;
-        // $config['database'] = env('database.default.khanza_db');
-        // $this->db = \Config\Database::connect($config);
-        // $this->forge = \Config\Database::forge($this->db);
+        $config = new \Config\Database()->default;
+        $config['database'] = env('database.default.khanza_db');
+        $db = \Config\Database::connect($config);
+        $db->query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
+        $prefix = APPPATH . 'Core/Database/Encrypt/';
 
-        // $this->db->query('CREATE EXTENSION IF NOT EXISTS pgcrypto;');
-        // $this->db->query(file_get_contents(self::PATH . 'rename_original_table.sql'));
-        // $this->db->query(file_get_contents(self::PATH . 'create_encrypted_table.sql'));
-        // $this->db->query(file_get_contents(self::PATH . 'create_encrypted_view.sql'));
-    }
-    
-    #[\Override()]
-    public function down(): void
-    {
-        // $config = new \Config\Database()->default;
-        // $config['database'] = env('database.default.khanza_db');
-        // $this->db = \Config\Database::connect($config);
-        // $this->forge = \Config\Database::forge($this->db);
-
-        // $this->db->query(file_get_contents(self::PATH . 'drop_function.sql'));
-        // $this->db->query(file_get_contents(self::PATH . 'drop_encrypted_view.sql'));
-        // $this->db->query(file_get_contents(self::PATH . 'drop_encrypted_table.sql'));
-    }
-
-    #[\Override()]
-    public function dependencies(): array {
-        return [];
+        $sql = match ($arg) {
+            'rename_table'   => 'rename_original_table.sql',
+            'create_table'   => 'create_encrypted_table.sql',
+            'create_view'    => 'create_encrypted_view.sql',
+            'drop_function'  => 'drop_function.sql',
+            'drop_view'      => 'drop_encrypted_view.sql',
+            'drop_table'     => 'drop_encrypted_table.sql',
+            'original_table' => 'rename_structures_table_to_original.sql',
+            default => die("Argument {$arg} not found"),
+        };
+        $db->query(file_get_contents($prefix . $sql));
     }
 }
