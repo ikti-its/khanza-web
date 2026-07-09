04a95177c94bfecdb1455e006e4d4c6c1845d154
Aliyah <aliyahawwaliyah@gmail.com>
2026-07-09 19:57:59 +0700

    Fix: remove trailing decimal zeros from money field

--- app/Views/components/form/isian/uang.php
+++ app/Views/components/form/isian/uang.php
@@ -3,15 +3,23 @@
      * @var int|string $id
      * @var string $column
      * @var string|int|float $value
-     * @var 0|1 $req 
+     * @var 0|1 $req
      */
+
+    $displayValue = $value;
+    if ($value !== '' && $value !== null && is_numeric($value)) {
+        $displayValue = rtrim(rtrim(sprintf('%.4F', (float) $value), '0'), '.');
+        if ($displayValue === '') {
+            $displayValue = '0';
+        }
+    }
 ?>
 
 <input
     id="<?= $id ?>"
     type="number"
     name="<?= $column ?>"
-    value="<?= $value ?>"
+    value="<?= $displayValue ?>"
     class="border border-gray-300 text-gray-900 text-sm rounded-lg p-2 w-full md:w-1/4 dark:border-gray-600 dark:text-white"
     placeholder="0" v
     <?= $req  === 1   ? 'required' : ''; ?>
