4b63cd36c81f93158862af988b609786ba156be2
qoyyimil <qoyyimillj@gmail.com>
2026-07-14 08:38:21 +0700

    Refactor: allow custom modal_id/extra_fields in delete modal to prevent DOM id collisions when a page lists rows from multiple tables

--- app/Views/components/aksi/hapus_form.php
+++ app/Views/components/aksi/hapus_form.php
@@ -1,6 +1,7 @@
-<div id="modelConfirm-<?= $id ?>" class="fixed hidden z-[70] inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4 ">
+<?php $modal_id ??= $id; ?>
+<div id="modelConfirm-<?= $modal_id ?>" class="fixed hidden z-[70] inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4 ">
     <div class="relative top-40 mx-auto shadow-xl rounded-md bg-white max-w-md">
-        <?= view('components/aksi/hapus_form_close', ['id' => $id]) ?>
+        <?= view('components/aksi/hapus_form_close', ['modal_id' => $modal_id]) ?>
         <div class="p-6 pt-0 text-center">
             <div class="flex justify-center mb-6">
                 <!-- Container for SVG, centered -->
@@ -11,8 +12,10 @@
                 Apakah anda yakin untuk menghapus data ini?
             </h3>
             <?= view('components/aksi/hapus_form_opsi', [
-                'modul_path' => $modul_path,
-                'id'         => $id
+                'modul_path'   => $modul_path,
+                'id'           => $id,
+                'modal_id'     => $modal_id,
+                'extra_fields' => $extra_fields ?? [],
             ]) ?>
         </div>
     </div>
